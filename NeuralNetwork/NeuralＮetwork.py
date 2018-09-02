21f877d1aaca00193d5ba16bb8095d80af100d24
alan <rlalan@outlook.com>
2018-09-02 23:24:53 +0800

    update neural network

--- NeuralNetwork/NeuralＮetwork.py
+++ NeuralNetwork/NeuralＮetwork.py
@@ -9,34 +9,51 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-def perception(weight, theta, x):
-    return sigmoid(weight * x + theta)
-
-
 class Perception(object):
-    def __init__(self, weight=0, theta=0):
-        # super(Perception, self).__init__(*args))
-        self.weight = weight
+    def __init__(self, theta=0, bias=0):
         self.theta = theta
+        self.bias = bias
 
-    def pass
+    def percept(self, x):
+        return sigmoid(np.sum(self.theta.dot(x.T)) + self.bias)
 
 
 class Layer(object):
-    def __init__(self, perception_num, x):
-        # super(Layer, self).__init__(*args))
-        self.perception = []
+    def __init__(self, perception_num=3):
+        self.perceptions = [Perception() for i in perception_num]
+
+    def forward(self, x):
+        forward_layer_output = []
+        for perception in self.perceptions:
+            layer_output.append(perception.percept(x))
+        return np.array(forward_layer_output)
+
+    def backward(self, x):
+        pass
 
 
 class NeuralNetwork(object):
     def __init__(self, X_train, Y_train):
-        # super(NerualNet, self).__init__(*args))
         self.X_train = np.array(X_train, dtype=float)
         self.Y_train = np.array(Y_train, dtype=float)
         self.inputSize = self.X_train.shape[-1]
         self.outputSize = self.Y_train.shape[-1]
+        self.inputLayer = Layer(self.inputSize)
+        self.outputLayer = Layer(self.outputSize)
         self.hidden_layers = []
-        pass
+        self.layers = self.combo_layers()
+
+    def combo_layers(self):
+        layers = [self.inputLayer]
+        if len(self.hidden_layers) != 0:
+            layers.extend(self.hidden_layers)
+        layers.append(self.outputLayer)
+        return layers
+
+    def train(self):
+        layer_ouput = self.layers[0].forward(self.X_train)
+        for layer in self.layers[1:]:
+            layer_ouput = layer.forward(layer_ouput)
 
 
 def main():
